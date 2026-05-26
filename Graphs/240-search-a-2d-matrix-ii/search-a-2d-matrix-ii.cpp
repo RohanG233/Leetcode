3086ea8b6ffe1b3165ae6e1e57d90e65411e6445
Rohan G <113969234+RohanG233@users.noreply.github.com>
2026-05-27 01:38:59 +0530

    Time: 55 ms (51.08%) | Memory: 18.7 MB (67.88%) - LeetSync

--- Graphs/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ Graphs/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0; i<matrix.size(); i++)
-        {
-            int low = 0;
-            int high = matrix[i].size()-1;
+        // Staircase Search
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-            while(low <= high)
+        int row = 0;
+        int col = n - 1;
+
+        while(row < m && col >= 0)
+        {
+            if(matrix[row][col] == target)
             {
-                int mid = low + (high - low) / 2;
-                if(matrix[i][mid] == target)
-                {
-                    return true;
-                }
-                else if(matrix[i][mid] > target)
-                {
-                    high = mid - 1;
-                }
-                else
-                {
-                    low = mid + 1;
-                }
+                return true;
+            }
+            else if(matrix[row][col] > target)
+            {
+                col = col - 1;
+            }
+            else
+            {
+                row = row + 1;
             }
         }
         return false;
